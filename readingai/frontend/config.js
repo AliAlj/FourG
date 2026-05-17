3a777da348321f7ebe1003fa41ccdac52b682726
AliAlj <alimjahmi@gmail.com>
2026-05-17 11:10:47 -0400

    restore config and Render backend URLs

---
+++ readingai/frontend/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  AZURE_KEY: "3jUKnlzXojp4XxOPTXJCd4BUX9x8DibnmVPCc8RlnxcerAMxVkuXJQQJ99CEACYeBjFXJ3w3AAAYACOGZJnx",
+  AZURE_REGION: "eastus",
+  IBM_PROJECT_ID: "eeea5b6b-d052-40dd-8a46-eeba801ccb5b",
+  IBM_API_KEY: "iQA7ph20PEFnB_pyEAdyCqkPnCnfcrqAX9JoDoVjcIMf",
+  WX_URL: "https://us-south.ml.cloud.ibm.com",
+  MODEL_ID: "ibm/granite-4-h-small",
+  SUPABASE_URL: "https://rdrlivuqsuxaaehikrwg.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_VrX4vm_7npxw8XsOYFWNyg_a3FA7rDz",
+  GOOGLE_BOOKS_KEY: "AIzaSyDJE2qfsrYLA5ttbrUHOm1BjdzKG7WZrI8",
+  GOOGLE_AI_KEY: "",
+  GOOGLE_TTS_KEY: "",
+  GROQ_API_KEY: ""
+};
